2392b685682dd2d50837f60e19f28de0859e2545
GiangLeDCT123 <lehoanggiang0405@gmail.com>
2025-04-10 07:33:34 +0700

    Delete KhachHang/web directory

--- KhachHang/web/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "ql_bangiay";
-$con = mysqli_connect($servername, $username, $password, $dbname);
-
-mysqli_set_charset($con,'utf8');
